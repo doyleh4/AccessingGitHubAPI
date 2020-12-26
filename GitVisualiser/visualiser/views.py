f4614b4ec33a001cdd781b9e6418135588e546c5
doyleh4 <60394085+doyleh4@users.noreply.github.com>
2020-12-26 19:24:05 +0000

    Commiting initial access to GitHub API
    This commit provides basic functionality to the GitHub API. It is a static website and outputs basic API data. I intend on adding a search bar to search for a user and also more complex GitHub API data usage. Also updated the README file

--- GitVisualiser/visualiser/views.py
+++ GitVisualiser/visualiser/views.py
@@ -1,17 +1,52 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests 
+from github import Github
+import json
 # Create your views here.
 
 Git_user_data = [
-        {'user' : 'hdoyle1999',
-        'repo' : 'HarryDoyle99',
-        'date_created' : '21/6/2020'
+        {'login' : None,
+        'name' : None,
+        'location' : None,
+        'no_of_repos': None,
+        'repos' : [],
+        'date_created' : None
         }
     ]
 
+####
+# inputs
+####
+yourUsername = 'doyleh4'
+
+# from https://github.com/user/settings/tokens
+token = '1cac1dbc068a4e45440aa8893989d16ffa6a8161'
+
+gh_session = requests.Session()
+gh_session.auth = (yourUsername, token)
+
+r = gh_session.get(url = 'https://api.github.com/rate_limit', params = {})
+data = r.json()
+print (data)
+
+username = "torvalds"
+url = f"https://api.github.com/users/{username}"
+user_data = gh_session.get(url).json()
+
 def home(request):
-    Git_user_data[0]['user'] = 'doyleh4'
+    g = Github(token)
+    #g = Github()
+    user = g.get_user(username)
+
+    Git_user_data[0]['login'] = user_data['login']
+    Git_user_data[0]['name'] = user_data['name']
+    Git_user_data[0]['location'] = user_data['location']
+    Git_user_data[0]['no_of_repos'] = str(user.get_repos().totalCount)
+    for repo in user.get_repos():
+        Git_user_data[0]['repos'].append(repo.full_name)
     
+    #Git_user_data[0]['repos'] = user.get_repos()
 
     data = {
         'Git_user_data' : Git_user_data
@@ -20,3 +55,66 @@ def home(request):
 
 def about(request):
     return render(request, 'visualiser/about.html', {'title':'About'})
+
+
+# import requests 
+# from github import Github
+# from pprint import pprint
+
+# ####
+# # inputs
+# ####
+# yourUsername = 'doyleh4'
+
+# # from https://github.com/user/settings/tokens
+# token = '1cac1dbc068a4e45440aa8893989d16ffa6a8161'
+
+# gh_session = requests.Session()
+# gh_session.auth = (yourUsername, token)
+
+# r = gh_session.get(url = 'https://api.github.com/rate_limit', params = {})
+# data = r.json()
+# print (data)
+
+# username = "doyleh4"
+# url = f"https://api.github.com/users/{username}"
+# user_data = gh_session.get(url).json()
+# pprint(user_data)       #using pprint() as regular print() prints it with no line breaks
+
+# print("-----------------------------------------------------------------------------------------------------------------")
+# #using PyGitHub to get this data thee resst is using requests still havent decided what i want to do for most of this 
+# g = Github(token)
+# #g = Github()
+# user = g.get_user(username)
+# login = "Not applied"
+# name = "Not applied"
+# location = "Not applied"
+# if (user.login is not None):
+#     login = user.login
+#     print("Login: " + login)
+# if (user.name is not None):
+#     name = user.name
+#     print("Name: " + name)
+# if (user.location is not None):
+#     location = user.location
+#     print("Name: " + location)
+# print("-----------------------------------------------------------------------------------------------------------------")
+# print('Total number of public repositories: ' + str(user.get_repos().totalCount))
+# for repo in user.get_repos():
+#     print(repo)
+
+# print("-----------------------------------------------------------------------------------------------------------------")
+# print("Total followers: " + str(user.get_followers().totalCount))
+
+# print("-----------------------------------------------------------------------------------------------------------------")
+# url = "https://api.github.com/repos/torvalds/linux/commits"
+# linux_repo = gh_session.get(url)
+# print("Total commmits to Linux repository: " + str(len(linux_repo.content)))
+
+# data = [
+#     {
+#     'user' : login,
+#     'name' : name,
+#     'location' : location
+#     }
+# ]
--- README.md
+++ README.md
@@ -1,19 +1,23 @@
 # GIT VISUALSIER TOOL
 The first inital commit of my Git Visualiser tool.
-The folder contains my django project.
+The folder contains my django project.  
 (This commit has no API functionality yet. It is simply the django functioning project for now.)
 
 ## myenv
-To create a virtual enviroment for this all you need to do is 
--pip install virtualenv
-then cd to the folder
--virtualenv myenv
-and simply 
--\env\Scripts\activate
-Then now the venv is set up and active just install django into the venv
--pip install django
-
-I intend on creating a terminal script that will do all this for you and also creating a docker instance to be the host.
+To create a virtual enviroment for this all you need to do is   
+-pip install virtualenv  
+then cd to the folder  
+-virtualenv myenv  
+and simply   
+-\env\Scripts\activate  
+Then now the venv is set up and active just install django into the venv  
+-pip install django  
+-pip install requests  
+-pip install pygithub  
+  
+I intend on creating a terminal script that will do all this for you and also creating a docker instance to be the host.  
 
 ## Git Visualiser
-Is the folder containg my django project. I used django for 2 reasons, 1) I had aready expermiented with using the API tools in python so i wanted to use that and 2) I wanted to experiment with more packages in Python and django was one that I really wnated to try as Pythyon is my favouruite language and iI wanted to learn how to tie front end and back end into Python.
+Is the folder containg my django project. I used django for 2 reasons, 1) I had aready expermiented with using the API tools in python so i wanted to use that and 2) I wanted to experiment with more packages in Python and django was one that I really wnated to try as Pythyon is my favouruite language and iI wanted to learn how to tie front end and back end into Python.  
+To run this simply enter   
+-python manage.py runserver 
